1cf28d9d7e6f4b78e302af72f13dd61bca7857ed
issac <523529051@qq.com>
2020-07-02 10:37:37 +0800

    Initial commit

---
+++ issaclibrary/src/main/java/com/hy/issaclibrary/T.java
@@ -0,0 +1,22 @@
+package com.hy.issaclibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * @author issac
+ * @version 1.0.0
+ * @Description
+ * @Date 2020/7/2 9:59
+ */
+public class T {
+    public static void show(Context context,String id ,String name){
+        Student s=new Student(id,name);
+        Gson g=new GsonBuilder().create();
+        String ss=g.toJson(s);
+        Toast.makeText(context,ss,Toast.LENGTH_LONG).show();
+    }
+}
